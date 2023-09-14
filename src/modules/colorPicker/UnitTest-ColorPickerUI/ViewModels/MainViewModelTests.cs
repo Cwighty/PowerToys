e88e9b37fb94f9e9584b442f9740b85467f6663c
Drew Gordon <andrewbengordon@gmail.com>
2023-09-14 16:02:24 -0600

    WIP AddColorsOnMouseDownTest

---
+++ src/modules/colorPicker/UnitTest-ColorPickerUI/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Drawing;
+using ColorPicker.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.ColorPicker.UnitTests.ViewModels;
+
+[TestClass]
+public class MainViewModelTests
+{
+    [TestMethod]
+    public void AddColorsOnMouseDownTest()
+    {
+        var mouseInfoProvider = new MouseInfoProviderFake();
+        var userSettings = new UserSettingsFake();
+        var viewModel = new MainViewModel(mouseInfoProvider, null, null, null, userSettings, System.Threading.CancellationToken.None);
+
+        mouseInfoProvider.TriggerColorChange(Color.Red);
+
+        Assert.Equals(userSettings.ColorHistory.Count, 1);
+    }
+}
---
+++ src/modules/colorPicker/UnitTest-ColorPickerUI/ViewModels/MouseInfoProviderFake.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Drawing;
+using ColorPicker.Mouse;
+
+namespace Microsoft.ColorPicker.UnitTests.ViewModels;
+
+public class MouseInfoProviderFake : IMouseInfoProvider
+{
+    public System.Windows.Point CurrentPosition => throw new NotImplementedException();
+
+    public Color CurrentColor => throw new NotImplementedException();
+
+    public event EventHandler<Color> MouseColorChanged = (sender, e) => { };
+
+    public event EventHandler<System.Windows.Point> MousePositionChanged = (sender, e) => { };
+
+    public event EventHandler<Tuple<System.Windows.Point, bool>> OnMouseWheel = (sender, e) => { };
+
+    public event MouseUpEventHandler OnMouseDown = (sender, e) => { };
+
+    public void TriggerColorChange(Color color)
+    {
+        MouseColorChanged?.Invoke(this, color);
+        OnMouseDown?.Invoke(this, default);
+    }
+}
